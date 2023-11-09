a531e09ac78c4fdb13850f1030d4a86ca87a6c0a
Wells <ll0918900@gmail.com>
2023-11-10 00:57:39 +0800

    feat: implement first version

---
+++ Chapter.4.3.H.員工資料表存取/Chapter.4.3.H.員工資料表存取/Program.cs
@@ -0,0 +1,7 @@
+﻿using Chapter._4._3.H.員工資料表存取.Domain.Database.Proxy;
+
+var protectionDatabaseProxy = new ProtectionDatabaseProxy();
+var employee = protectionDatabaseProxy.GetEmployeeById(1);
+var subordinates = employee!.Subordinates;
+Console.WriteLine($"Employee: {employee.Name}");
+Console.WriteLine($"Subordinates: {string.Join(", ", subordinates!.Select(x => x.Name))}");
